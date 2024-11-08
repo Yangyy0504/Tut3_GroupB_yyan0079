312e2d109c031e14a905e95704836049e115defc
Yangyy0504 <yyan0079@uni.sydney.edu.au>
2024-11-08 12:15:55 +1100

    Update sketch 1 .js
    I've removed some of the buildings from the base code, and changed the color of the horizontal road in the picture to a single color.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-// Define grid data. roadMetrics is a 2D array where each sub-array represents a row on the map.
-// Numbers indicate different colors for blocks: 0-blank, 1-yellow, 2-red, 3-blue, 4-gray.
 let roadMetrics = [
   [0,1,0,2,0,0,1,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,2,0,1,0,2,0],
   [1,3,1,1,1,1,1,1,3,1,4,1,1,1,1,1,4,1,1,1,1,1,1,1,4,1,4,1,4,1,1,1,1,4,4,1,1,1,4,3,1,4,1,3,4,1,4],
@@ -11,7 +9,7 @@ let roadMetrics = [
   [0,1,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,1,0,0,0,0,0,0,0,0,0,0,0,3,0,1,0,0,0,1,0],
   [0,1,0,4,0,0,1,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,4,0,0,0,0,0,0,0,0,0,0,0,1,0,4,0,0,0,4,0],
 
-  [1,2,1,1,1,1,3,1,4,4,2,1,4,1,3,1,1,4,1,1,1,1,1,1,4,3,1,2,4,1,1,1,1,1,1,4,1,1,4,3,4,1,2,1,4,3,1],
+ [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 
   [0,1,0,4,0,0,1,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,4,0,0,0,0,0,0,0,0,0,0,0,1,0,4,0,4,0,4,0],
   [0,1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0],
@@ -23,8 +21,7 @@ let roadMetrics = [
   [0,1,0,1,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,4,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0],
   [0,4,0,1,0,0,3,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0],
 
-  [1,2,1,3,1,1,4,2,1,1,4,3,1,1,4,1,3,1,2,1,1,1,3,1,1,2,1,3,1,1,2,1,1,1,2,1,1,1,1,3,1,2,1,4,3,4,1],
-
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
   [0,0,0,1,0,0,3,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0],
   [0,0,0,1,0,0,4,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,4,0,1,0],
   [0,0,0,1,0,0,4,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,1,0,4,0],
@@ -37,61 +34,51 @@ let roadMetrics = [
   [0,0,0,1,0,0,3,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,4,0],
   [0,0,0,4,0,0,1,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,2,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0],
 
-  [1,1,4,3,1,1,3,1,1,1,1,3,1,1,4,1,2,1,1,2,4,4,2,1,1,4,1,4,1,1,1,2,1,3,4,4,3,1,1,2,1,1,1,3,4,2,1],
+ [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 
   [0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0],
   [0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,1,0,0,0,2,0,0,0,0,0,0,0,4,0,0,0,0,0,4,0],
 
-  [1,1,4,2,1,1,3,1,1,4,1,3,1,1,4,1,1,3,1,1,2,1,1,3,1,4,1,4,1,1,1,4,1,3,1,3,1,1,1,3,1,1,1,2,4,1,2],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 
   [0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,3,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,4,0,4,0],
   [0,0,0,4,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0],
   [1,3,1,2,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,4,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0],
   [0,0,0,4,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,4,0,2,0],
-  [0,0,0,2,1,1,2,1,1,4,1,3,1,1,1,4,1,1,3,1,1,2,1,3,1,4,2,4,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,4,0,4,0],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
   [0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,3,0,3,0],
   [1,3,1,2,0,0,3,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,2,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0],
   [0,0,0,4,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,4,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0],
   [0,0,0,1,0,0,2,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,3,1,1,1,4,1,2,0],
   [0,0,0,1,0,0,1,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0],
   
-  [1,4,1,3,1,1,3,1,1,2,4,3,1,1,1,4,1,1,3,1,2,1,3,2,1,3,1,2,1,3,1,2,1,3,1,1,4,1,1,2,1,1,3,1,1,2,1],
+ [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
   [0,0,0,4,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,4,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0],
   [0,0,0,1,0,0,3,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,4,0],
   [0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0],
   [0,0,0,4,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,3,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0],
-  [1,1,4,2,1,1,3,1,1,2,1,3,1,4,1,1,4,1,3,1,3,2,2,2,1,4,1,4,1,2,1,3,1,3,1,1,4,1,1,3,1,1,2,1,1,2,1],
+ [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
   [0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,1,0],
   [0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0]
 ];
-// Define arrays to store blocks of different colors
+
 let redBlocks = [];
 let yellowBlocks = [];
 let blueBlocks = [];
 let grayBlocks = [];
-// Variable to hold the canvas object
 let canvas;
 
-
-// Resizes the canvas and centers it when the window is resized
-//This technique is from https://p5js.org/reference/p5/windowResized/
 function windowResized() {
   resizeCanvas(558, 558);
   canvas.position(windowWidth / 2 - width / 2, windowHeight / 2 - height / 2);
 }
 
-// setup function is called once at the start of the program, initializes the canvas and creates blocks
 function setup() {
-  // Create canvas and center it in the window
-   canvas = createCanvas(558, 558);
+  canvas = createCanvas(558, 558);
   canvas.position(windowWidth / 2 - width / 2, windowHeight / 2 - height / 2);
-  //for Loop through each row in roadMetrics
   for (let i = 0; i < roadMetrics.length; i++) {
     let row = roadMetrics[i];
-    console.log(row.length);  
-     // Loop through each element in the row
     for (let j = 0; j < row.length; j++) {
-      // Based on the element's value, create blocks of different colors and add them to the respective arrays
       switch(row[j]) {
         case 1:
           yellowBlocks.push(new Block(color(225, 201, 41), 558 / row.length * j, 558 / 50 * i, 558 / row.length, 558 / 50));
@@ -111,7 +98,7 @@ function setup() {
     }
   }
 }
-// Define a Block class to represent the color and position of each block
+
 class Block {
   constructor(color, x, y, width, height) {
     this.color = color;
@@ -121,8 +108,7 @@ class Block {
     this.height = height;
   }
 }
-// Function to draw all blocks
-// This technique is from https://p5js.org/reference/p5/draw/
+
 function drawAllBlocks() {
   for (let redBlock of redBlocks) {
     fill(redBlock.color);
@@ -146,75 +132,43 @@ function drawAllBlocks() {
   }
 }
 
-// Main drawing function called on each frame
 function draw() {
   background(242, 243, 238);
-  drawAllBuildings()
+  drawAllBuildings();
   drawAllBlocks();
+  drawAxisCoordinates(); 
 }
-// Function to draw all buildings
-function drawAllBuildings(){
-  drawBuildings(83,33.90,48,18,color(225, 201, 41));
-  drawBuildings(92,21.90,24,68,color(175,57,43));
-  drawBuildings(92,52,24,13,color(217, 214, 209));
 
+function drawAllBuildings() {
+  drawBuildings(83, 33.90, 48, 18, color(225, 201, 41));
+  drawBuildings(92, 21.90, 24, 68, color(175, 57, 43));
+  drawBuildings(92, 52, 24, 13, color(217, 214, 209));
   
-  drawBuildings(152,21.90,44,52,color(175,57,43));
-  drawBuildings(162,38.90,23,20,color(217, 214, 209));
-  drawBuildings(152,73.90,44,15.8,color(217, 214, 209));
- 
-
-  drawBuildings(498.5,56,36,20,color(76,102,197));
-
-  drawBuildings(47,112,36,33,color(76,102,197));
-
-  drawBuildings(83,160,47.6,33,color(225, 201, 41));
-  drawBuildings(100,168,16,14,color(217, 214, 209));
-
-
-  drawBuildings(360,100,50,100.5,color(76,102,197));
-  drawBuildings(360,130,50,48,color(175,57,43));
-  drawBuildings(375,145,25,20,color(225, 201, 41));
-
-  drawBuildings(486.5,112,36,33,color(175,57,43));
-
-  drawBuildings(95.5,212,22.5,35,color(225, 201, 41));
-  drawBuildings(100,222,13,12,color(217, 214, 209));
-
-  drawBuildings(83,277.2,47.6,35,color(175,57,43));
-
-  drawBuildings(166,256,36,56.6,color(225, 201, 41));
-  drawBuildings(166,270,36,42.6,color(76,102,197));
-  drawBuildings(173,283,21.5,16,color(225, 201, 41));
-
-  drawBuildings(225,212,36,101,color(225, 201, 41));
-  drawBuildings(225,268,36,25,color(217, 214, 209));
-
-  drawBuildings(392,256,43,77,color(175,57,43));
-  drawBuildings(402,275,24,20,color(217, 214, 209));
-  drawBuildings(392,333,43,15,color(217, 214, 209));
-
-  drawBuildings(475,273,60,25,color(225, 201, 41));
-  drawBuildings(499,273,12,25,color(175,57,43));
-
-  drawBuildings(47,368.5,36,33.5,color(76,102,197));
-
-  drawBuildings(475,368.5,36,33.5,color(76,102,197));
-  drawBuildings(475,402,36,18,color(225, 201, 41));
-  drawBuildings(475,420,36,33.5,color(175,57,43));
-
-  drawBuildings(83,430,47.6,39,color(225, 201, 41));
-  drawBuildings(100,439,16,13,color(217, 214, 209));
-
-  drawBuildings(0,485,36,14,color(225, 201, 41));
-  drawBuildings(18,485,14,14,color(175,57,43));
-
-  drawBuildings(249,528,36,22,color(175,57,43));
-
 }
-// Function to draw individual buildings
-function drawBuildings(x,y,width,height,color){
+
+function drawBuildings(x, y, width, height, color) {
   fill(color);
-  rect(x,y,width,height);
-  noStroke();;
+  rect(x, y, width, height);
+  noStroke();
+}
+
+function drawAxisCoordinates() {
+  fill(0);
+  textSize(12);
+
+  let numRows = roadMetrics.length;
+  let numCols = roadMetrics[0].length;
+
+  let cellWidth = width / numCols;
+  let cellHeight = height / numRows;
+
+
+  for (let j = 0; j < numCols; j++) {
+    text(`${j}`, j * cellWidth + cellWidth / 2 - 6, 10);
+  }
+
+ 
+  for (let i = 0; i < numRows; i++) {
+    text(`${i}`, 2, i * cellHeight + cellHeight / 2 + 4);
+  }
 }
